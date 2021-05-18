fc7dd54f83c0376b0ad5941ef1b753086028a73c
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-18 03:33:18 -0500

    fixed route order

--- src/urls/urls.router.js
+++ src/urls/urls.router.js
@@ -3,15 +3,15 @@ const controller = require("./urls.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
-  .route("/")
-  .get(controller.list)
-  .post(controller.create)
-  .all(methodNotAllowed);
-
-router
   .route("/:urlId")
   .get(controller.list)
   .put(controller.update)
+  .all(methodNotAllowed);
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
   .all(methodNotAllowed);
 
 module.exports = router;
--- src/uses/uses.router.js
+++ src/uses/uses.router.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const controller = require("./uses.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
-
 router
   .route("/:useId")
   .get(controller.list)
   .delete(controller.delete)
   .all(methodNotAllowed);
+
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
